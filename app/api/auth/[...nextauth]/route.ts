cdb2ad49e84d775f05e7e3c960754265d060b3f8
Daniel Nyong <danielnyong66@gmail.com>
2023-11-27 00:48:39 +0100

    .

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,45 +1,5 @@
-import requests from "@/lib/utils/requests";
-import NextAuth, { SessionStrategy } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-
-export const authOptions = {
-  pages: {
-    signIn: "/login",
-    signOut: "/logout",
-  },
-  session: {
-    strategy: "jwt" as SessionStrategy,
-  },
-  callbacks: {
-    jwt: async ({ token, account }) => {
-      if (account?.id_token) {
-        token.jwtToken = account.id_token;
-      }
-      const apiResponse = await requests(
-        process.env.NEXT_PUBLIC_API + "/google/",
-        "POST",
-        { auth_token: token.jwtToken },
-        (error) => console.error("Login error:", error)
-      );
-      token.apiResponse = apiResponse;
-      return token;
-    },
-    session: async ({ session, token }: any) => {
-      session.accessToken = token.jwtToken;
-      session.apiResponse = token.apiResponse;
-      return session;
-    },
-    redirect({ url, baseUrl }) {
-      return baseUrl;
-    },
-  },
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-};
+import authOptions from "@/lib/utils/authOptions";
+import NextAuth from "next-auth";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ lib/utils/authOptions.ts
@@ -0,0 +1,42 @@
+import { SessionStrategy } from "next-auth";
+import requests from "./requests";
+import GoogleProvider from "next-auth/providers/google";
+
+export default {
+  pages: {
+    signIn: "/login",
+    signOut: "/logout",
+  },
+  session: {
+    strategy: "jwt" as SessionStrategy,
+  },
+  callbacks: {
+    jwt: async ({ token, account }: any) => {
+      if (account?.id_token) {
+        token.jwtToken = account.id_token;
+      }
+      const apiResponse = await requests(
+        process.env.NEXT_PUBLIC_API + "/google/",
+        "POST",
+        { auth_token: token.jwtToken },
+        (error) => console.error("Login error:", error)
+      );
+      token.apiResponse = apiResponse;
+      return token;
+    },
+    session: async ({ session, token }: any) => {
+      session.accessToken = token.jwtToken;
+      session.apiResponse = token.apiResponse;
+      return session;
+    },
+    redirect({ url, baseUrl }: any) {
+      return baseUrl;
+    },
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+};
