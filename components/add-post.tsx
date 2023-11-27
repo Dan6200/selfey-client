0b69c910ea46009743fb3c8e24194a32b28e97fa
Daniel Nyong <danielnyong66@gmail.com>
2023-11-27 03:29:47 +0100

    added webcam

--- components/add-post.tsx
+++ components/add-post.tsx
@@ -2,7 +2,9 @@
 import requests from "@/lib/utils/requests";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
+import { useCallback, useRef } from "react";
 import { useForm } from "react-hook-form";
+import Webcam from "react-webcam";
 import { Button } from "./ui/button";
 import { Card, CardHeader } from "./ui/card";
 import { Textarea } from "./ui/textarea";
@@ -11,8 +13,12 @@ export default function AddPost() {
   const { register, handleSubmit } = useForm();
   const session = useSession();
   const router = useRouter();
-  console.log(session);
+  const webcamRef = useRef(null);
 
+  const capture = useCallback(() => {
+    const imageSrc = (webcamRef as any).current?.getScreenshot();
+    console.log(imageSrc);
+  }, [webcamRef]);
   const submitHandler = async (router: any, data: any) => {
     const formData = new FormData();
     const { image, description } = data;
@@ -31,8 +37,17 @@ export default function AddPost() {
 
   return (
     <section className="flex flex-col items-center">
-      <Card className="w-[90%] md:w-[50%] h-96 p-16">
+      <Card className="w-[90%] md:w-[50%] h-[56rem] p-16">
         <CardHeader>
+          <Webcam
+            className="mb-8"
+            audio={false}
+            ref={webcamRef}
+            screenshotFormat="image/jpeg"
+          />
+          <Button className="mb-8" onClick={capture}>
+            Capture photo
+          </Button>
           <form
             className="flex flex-col h-64 justify-between"
             onSubmit={handleSubmit(submitHandler.bind(null, router))}
